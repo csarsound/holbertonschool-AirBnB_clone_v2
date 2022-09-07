1d817349578829e69a21013db54a44b3afdfa271
csarsound <csarsound@hotmail.com>
2022-09-07 11:26:39 -0500

    CorrectionSintaxis

--- models/user.py
+++ models/user.py
@@ -15,8 +15,8 @@ class User(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128)
-        last_name = Column(String(128)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
         places = relationship("Place", cascade="all, delete", backref="user")
         review = relationship("Review", cascade="all, delete", backref="user")
 
