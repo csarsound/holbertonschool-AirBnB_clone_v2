cdbe35ceb6fde29ff94b83ef5e342ddc02632e14
csarsound <csarsound@hotmail.com>
2022-09-07 11:18:23 -0500

    User

--- models/user.py
+++ models/user.py
@@ -17,8 +17,8 @@ class User(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
+        first_name = Column(String(128)
+        last_name = Column(String(128)
         places = relationship("Place", cascade="all, delete", backref="user")
         review = relationship("Review", cascade="all, delete", backref="user")
 
